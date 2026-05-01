3e824375f6fb5542431c2edaed04c2735bf74280
Matheus de Almeida <matheusoallmeida@gmail.com>
2026-05-01 18:42:25 -0300

    estrutura inicial do backend

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('cpf', 14)->unique();
+            $table->string('rg', 20)->nullable();
+            $table->date('data_nascimento')->nullable();
+            $table->string('telefone', 20);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'cliente'])->default('cliente');
             $table->rememberToken();
             $table->timestamps();
         });
