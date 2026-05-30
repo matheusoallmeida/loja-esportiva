dbd92f5a99170c3bcca107b4711bf42b70b279bb
Matheus de Almeida <matheusoallmeida@gmail.com>
2026-05-29 21:02:01 -0300

    libera rotas para testes frontend

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,4 @@
+```php
 <?php
 
 use App\Http\Controllers\ProfileController;
@@ -11,49 +12,88 @@ use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\VendaController;
 use App\Http\Controllers\CarrinhoController;
 use App\Http\Controllers\CheckoutController;
-use App\Http\Controllers\WelcomeController;
 
-Route::get('/', [WelcomeController::class, 'index']);
+/*
+|--------------------------------------------------------------------------
+| PÁGINA INICIAL
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/', function () {
+
+    $produtos = \App\Models\Produto::all();
+
+    return view('welcome', compact('produtos'));
+
+});
+
+/*
+|--------------------------------------------------------------------------
+| PRODUTO
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/produto/{produto}', [ProdutoController::class, 'show'])
     ->name('produto.show');
+
+/*
+|--------------------------------------------------------------------------
+| DASHBOARD
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+/*
+|--------------------------------------------------------------------------
+| ROTAS DE USUÁRIO LOGADO
+|--------------------------------------------------------------------------
+*/
 
-// ROTAS DE USUÁRIO LOGADO
 Route::middleware('auth')->group(function () {
 
+    // Perfil
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    // Endereços
     Route::resource('enderecos', EnderecoController::class);
 
+    // Carrinho
     Route::resource('carrinhos', CarrinhoController::class);
 
+    // Checkout
     Route::post('/checkout', [CheckoutController::class, 'finalizar'])
         ->name('checkout.finalizar');
+
 });
 
+/*
+|--------------------------------------------------------------------------
+| ROTAS LIBERADAS TEMPORARIAMENTE PARA TESTES
+|--------------------------------------------------------------------------
+|
+| Depois basta voltar o middleware admin.
+|
+*/
 
-// ROTAS SOMENTE ADMIN
-Route::middleware(['auth', 'admin'])->group(function () {
+Route::resource('users', UserController::class);
 
-    Route::resource('users', UserController::class);
+Route::resource('categorias', CategoriaController::class);
 
-    Route::resource('categorias', CategoriaController::class);
+Route::resource('tamanhos', TamanhoController::class);
 
-    Route::resource('tamanhos', TamanhoController::class);
+Route::resource('cidades', CidadeController::class);
 
-    Route::resource('cidades', CidadeController::class);
+Route::resource('produtos', ProdutoController::class)
+    ->except(['show']);
 
-    Route::resource('produtos', ProdutoController::class)
-        ->except(['show']);
-
-    Route::resource('vendas', VendaController::class);
-});
+Route::resource('vendas', VendaController::class);
 
 require __DIR__.'/auth.php';
+```
